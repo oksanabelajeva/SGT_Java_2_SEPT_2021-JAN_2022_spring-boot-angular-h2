948861228faeb5c9cb472ae66a97967bf8b4248c
Oxana <oksanabelajeva@inbox.lv>
2021-11-07 17:05:35 +0200

    Initial commit

---
+++ src/main/java/lv/oxana/springbootangularh2/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package lv.oxana.springbootangularh2.repository;
+
+import lv.oxana.springbootangularh2.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
---
+++ src/test/java/lv/oxana/springbootangularh2/SpringBootAngularH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.oxana.springbootangularh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
